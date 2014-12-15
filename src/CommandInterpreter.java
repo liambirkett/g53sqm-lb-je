7c88446557b3da2bbd2d99cf8a93d512b0160084
liambirkett <liam894@gmail.com>
2014-12-15 15:23:57 +0000

    Implemented some of STAT and IDEN in CommandInterpreter and added relevant JUnit tests.

--- src/CommandInterpreter.java
+++ src/CommandInterpreter.java
@@ -1,4 +1,77 @@
 
 public class CommandInterpreter {
 
+	
+	protected String output;
+	private String command;
+	public static boolean ISLOGGEDIN = false;
+	
+	//constructor
+	public CommandInterpreter(){
+		
+	}
+	
+	//handle the user's commands
+	public String handleInput(String input){
+		try{
+			//split up command into its constituent words
+			//first word is command
+			String[] result = input.split("\\s");
+		    command = result[0];
+		    
+		    	switch(command){
+				case "STAT":
+					output = stat();
+					break;
+				case "IDEN":
+					output = iden(result[1]);
+					break;
+		    	default:
+		    		output = "-ERR unknown command";
+		    		break;
+		    			
+		    	}
+		    
+			return output;
+		}catch(Exception e){
+			return "-ERR invalid argument";
+		}
+	}
+	
+	//handles the stat command
+	private String stat(){
+		
+		int noOfUsersLoggedIn = 0; //placeholder until functionality is there
+		String status = "OK. " + noOfUsersLoggedIn + " users logged in. You are logged ";
+		
+		
+		int noOfMsgsSent=0; //placeholder until functionality is there
+		
+		//checks if user is logged in
+		if(ISLOGGEDIN){
+			status += "in. No of msgs sent = " + noOfMsgsSent + ".";
+		}else{
+			status += "out.";
+		}
+		
+		return status;
+	}
+	
+	private String iden(String uname){
+		
+		//check that username is not taken
+		boolean unameTaken = false, unameHasIllegalCharacters = false; //placeholder until functionality is there
+		
+		if(unameTaken){
+			return "BAD. Username already taken. Try a different one.";
+			
+		}else if(unameHasIllegalCharacters){
+			char x = '*'; //placeholder until functionality is there
+			return "BAD. Username has illegal character " + x + ". Try a different one.";
+		}else{
+			//log the user in
+			ISLOGGEDIN = true;
+			return "OK. Welcome " + uname + ". Have a lot of fun...";
+		}
+	}
 }
--- src/TestCommandInterpreter.java
+++ src/TestCommandInterpreter.java
@@ -3,11 +3,31 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 
-public class TestCommandInterpreter {
+public class TestCommandInterpreter extends CommandInterpreter {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testHandleInputSTAT(){
+		
+		//stat test
+		ISLOGGEDIN=false;
+		String s = handleInput("STAT");
+		assertEquals(s, "OK. 0 users logged in. You are logged out.");
+	}
+	
+	@Test
+	public void testHandleInputUnknown(){
+		
+		//unknown command test
+		String s = handleInput("blah");
+		assertEquals(s, "-ERR unknown command");	
+	}
+	
+	@Test
+	public void testHandleInputIDEN(){
+		
+		//iden test
+		String s = handleInput("IDEN exampleUsername");
+		assertEquals(s, "OK. Welcome exampleUsername. Have a lot of fun...");
 	}
 
 }
